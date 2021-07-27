863db67f835af8fde005409fe5ad1ac85409dda4
Dominic <git@msrd0.de>
2021-07-27 19:43:17 +0200

    update jQuery to 3.6.0

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,6 @@
 ---
 ---
-{% include js/jquery-3.2.1.slim.min.js %}
+{% include js/jquery-3.6.0.slim.min.js %}
 {% include js/popper-1.12.9.min.js %}
 {% include js/bootstrap-4.0.0.min.js %}
 {% include js/highlight-rust.pack.js %}
